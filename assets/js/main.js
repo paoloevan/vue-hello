e2538a5b6eef4e366f7d323a4135fb2429aa2292
paoloevan <paoloev1900@gmail.com>
2022-10-26 14:26:56 +0200

    add class

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,15 @@
 //Bonus:
 //Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+
 const { createApp } = Vue
 
 createApp({
     data(){
         return{
             title: 'Hello Vue!',
-            imgUrl: 'https://www.monterail.com/hubfs/vue-optimized.png'
+            imgUrl: 'https://www.monterail.com/hubfs/vue-optimized.png',
+            jsClass: 'text-center my-3'
         }
     }
 }).mount('#app')
